e6aeae4b53a74d67ad0c6645b005d582e68d6590
Mohamed Humaam Athif <mohamed.humaam.athif@gmail.com>
2025-03-26 20:17:04 +0500

    did something again

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,1 @@
 import './bootstrap';
-import Alpine from 'alpinejs'
-import Livewire from 'livewire-v2'
-import FileUpload from '~/filament/forms/components/file-upload'
-import RichEditor from '~/filament/forms/components/rich-editor'
-
-window.Alpine = Alpine
-window.Livewire = Livewire
-
-Alpine.plugin(FileUpload)
-Alpine.plugin(RichEditor)
-
-Alpine.start()
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path';
 
 export default defineConfig({
-    base: process.env.ASSET_URL || '/',
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    resolve: {
-        alias: {
-            '~filament': path.resolve(__dirname, './vendor/filament/filament/resources/js'),
-        },
-    },
 });
