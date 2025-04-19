62d30af63944da9482c508a99c07350da3cebab1
Mohamed Humaam Athif <mohamed.humaam.athif@gmail.com>
2025-04-19 22:48:45 +0500

    removed size for test

--- database/migrations/2025_04_19_173109_modify_rooms_table.php
+++ database/migrations/2025_04_19_173109_modify_rooms_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('rooms', function (Blueprint $table) {
-            $table->dropColumn('size');
             $table->dropColumn('price');
             $table->decimal('price_per_night',  8, 2)->default(0)->after('description');
 //            $table->integer('capacity')->default(0)->after('price_per_night');
@@ -25,7 +24,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('rooms', function (Blueprint $table) {
-            $table->decimal('size', 8, 2)->default(0);
             $table->decimal('price', 8, 2)->default(0);
             $table->dropColumn('price_per_night');
 //            $table->dropColumn('capacity');
