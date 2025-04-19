abbc3c8dafb8b696857f7779f510fa83dcf1e166
Mohamed Humaam Athif <mohamed.humaam.athif@gmail.com>
2025-04-19 22:49:35 +0500

    removed price for test

--- database/migrations/2025_04_19_173109_modify_rooms_table.php
+++ database/migrations/2025_04_19_173109_modify_rooms_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('rooms', function (Blueprint $table) {
-            $table->dropColumn('price');
+//            $table->dropColumn('price');
             $table->decimal('price_per_night',  8, 2)->default(0)->after('description');
 //            $table->integer('capacity')->default(0)->after('price_per_night');
         });
@@ -24,7 +24,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('rooms', function (Blueprint $table) {
-            $table->decimal('price', 8, 2)->default(0);
+//            $table->decimal('price', 8, 2)->default(0);
             $table->dropColumn('price_per_night');
 //            $table->dropColumn('capacity');
         });
