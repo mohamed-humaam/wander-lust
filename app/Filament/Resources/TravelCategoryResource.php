4b5d3fa7d0641831c739cda374ef78ecffe58ed4
Mohamed Humaam Athif <mohamed.humaam.athif@gmail.com>
2025-04-30 15:01:50 +0500

    updated namespaces

--- app/Filament/Resources/TravelCategoryResource.php
+++ app/Filament/Resources/TravelCategoryResource.php
@@ -2,7 +2,7 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\TravelCategoryResource\Pages;
 use App\Models\Category;
 use Filament\Forms;
 use Filament\Forms\Components\Section;
